6cdd6ed2e22ca1a80a19da1b9ebd5ad06c5ee601
剑来 <jscptman@gmail.com>
2025-02-13 17:52:03 +0800

    improve: 2.1.rs

--- crates/chapter_2/src/bin/2.1.rs
+++ crates/chapter_2/src/bin/2.1.rs
@@ -1,30 +1,6 @@
 use std::io::{Read, Result as IoResult};
 use std::net::{TcpListener, TcpStream};
 
-enum HttpProtocol {
-    HTTP11,
-    HTTP2,
-}
-struct HttpMessage {
-    protocol: HttpProtocol,
-}
-enum HttpParseError {}
-impl HttpMessage {
-    fn new<T>(message: T) -> Self
-    where
-        T: AsRef<str>,
-    {
-        Self::parse_message(message.as_ref())
-            .unwrap_or_else(|e| panic!("http parse occurs an error"))
-    }
-
-    fn parse_message(message: &str) -> Result<Self, HttpParseError> {
-        Ok(Self {
-            protocol: HttpProtocol::HTTP11,
-        })
-    }
-}
-
 fn main() {
     start().unwrap_or_else(|e| {
         eprintln!("{}", e);
