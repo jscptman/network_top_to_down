54ab63643f7e71acbb1422ce288e19d4231536eb
剑来 <jscptman@gmail.com>
2025-03-14 16:23:52 +0000

    refactor: reinit project

--- crates/chapter_2/src/bin/2.1.rs
+++ crates/chapter_2/src/bin/2.1.rs
@@ -1,24 +1,2 @@
-use std::io::{Read, Result as IoResult};
-use std::net::{TcpListener, TcpStream};
-
-fn main() {
-    start().unwrap_or_else(|e| {
-        eprintln!("{}", e);
-    })
-}
-
-fn start() -> IoResult<()> {
-    let listener = TcpListener::bind("127.0.0.1:8085")?;
-    while let Ok((stream, addr)) = listener.accept() {
-        println!("got a connection from: {}", addr);
-        handle_request(stream)?
-    }
-    Ok(())
-}
-
-fn handle_request(mut req: TcpStream) -> IoResult<()> {
-    let mut buf = String::new();
-    let read_num = req.read_to_string(&mut buf)?;
-    println!("request:\n{}", &buf);
-    Ok(())
-}
+#![deny(warnings)]
+fn main() {}
