f9f6c59a0cc9bc850c8c17fb8f99f319b8f57cae
剑来 <jscptman@gmail.com>
2025-03-19 16:30:16 +0000

    feat: 2.1.rs

--- crates/chapter_2/src/bin/2.1.rs
+++ crates/chapter_2/src/bin/2.1.rs
@@ -1,2 +1,22 @@
-#![deny(warnings)]
-fn main() {}
+use http_body_util::Empty;
+use hyper::Request;
+use hyper::client::conn::http1::handshake;
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpStream;
+
+#[tokio::main]
+async fn main() {
+    let stream = TcpStream::connect("example.com:80").await.unwrap();
+    let io = TokioIo::new(stream);
+    let (mut sender, _conn) = handshake(io).await.unwrap();
+
+    // 使用 sender 发送请求
+    let req = Request::builder()
+        .uri("/")
+        .body(Empty::<&[u8]>::new())
+        .unwrap();
+    let response = sender.send_request(req).await.unwrap();
+
+    // 处理响应
+    println!("Response status: {}", response.status());
+}
